756e12da82fdab3995fd7cb9e35973f52af63bbe
Vinicius <vinicius.castro@paretogroup.com.br>
2022-05-30 00:50:57 -0300

    feat: update users name

--- src/shared/resources/users.ts
+++ src/shared/resources/users.ts
@@ -1,12 +1,12 @@
 export const members = {
-    '1': "Lucas Sátiro", 
+    '1': "Lucas Joaquim",
     '2': "Marcus Maninho",
     '3': "Miguel Oliveira",
-    '5': "Pedro Campos",
+    '5': "Pedro Intruso 1 Campos",
     '8': "Rafael Ferrari",
     '9': "Raphael Carvalho",
     '11': "Vinicius Castro",
     '16': "Intruso 1",
     '17': "Intruso 2",
     '18': "Duplamente Intruso",
-}
+};
