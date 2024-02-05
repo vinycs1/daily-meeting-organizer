e08d021756c4c8cae71a5fa0f20f52c4f175e253
Vinicius Castro <vinycastro1@gmail.com>
2024-02-05 11:40:22 -0300

    Update users.ts

--- src/shared/resources/users.ts
+++ src/shared/resources/users.ts
@@ -14,7 +14,7 @@ export const members: Record<string, string> = {
     '14': "Eduardo Resser",
     '15': "Roberto Junior",
     '16': "Pedro Vido",
-    '17': "Erick Pimentel",
+    '17': "Erick Eloi",
     '25': "Intruso 1",
     '26': "Duplamente Intruso",
     '27': "Duplamente e um cadinho a mais Intruso",
